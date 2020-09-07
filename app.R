12050872a0fd2551a26b45af2ee202a85a55caad
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-09-07 19:16:01 +0100

    improve the plot

--- app.R
+++ app.R
@@ -1,5 +1,4 @@
 library(googlesheets4)
-library(plotly)
 library(shiny)
 library(simputation)
 library(tidyverse)
@@ -21,42 +20,40 @@ energy <- energy %>%
     impute_lm(electricity ~ date)
 
 energy <- energy %>%
-    arrange(date) %>%
-    mutate(
-      electricity_unit_rate = 15.886, # 2018 - 2019
-      gas_unit_rate         = 3.869,  # 2018 - 2019
-      electricity_unit_rate = 15.61,  # 2019 - 2020 19.33 standing charge
-      gas_unit_rate         = 3.33,   # 2019 - 2020 19.23 standing charge
-      electricity_unit_rate = 13.548, # 2020 - 2021 18.073
-      gas_unit_rate         = 2.607   # 2020 - 2021 10.62
-    ) %>%
-    mutate(
-      gas_cost         = gas * gas_unit_rate,
-      electricity_cost = electricity * electricity_unit_rate
-    ) %>%
-    mutate(
-      total_gas              = cumsum(gas),
-      total_electricity      = cumsum(electricity),
-      total_gas_cost         = cumsum(gas_cost),
-      total_electricity_cost = cumsum(electricity_cost),
-    )
+    arrange(date) #%>%
+    #mutate(
+    #  electricity_unit_rate = 15.886, # 2018 - 2019
+    #  gas_unit_rate         = 3.869,  # 2018 - 2019
+    #  electricity_unit_rate = 15.61,  # 2019 - 2020 19.33 standing charge
+    #  gas_unit_rate         = 3.33,   # 2019 - 2020 19.23 standing charge
+    #  electricity_unit_rate = 13.548, # 2020 - 2021 18.073
+    #  gas_unit_rate         = 2.607   # 2020 - 2021 10.62
+    #) %>%
+    #mutate(
+    #  gas         = gas * gas_unit_rate,
+    #  electricity = electricity * electricity_unit_rate
+    #)
 
 tidy_energy <- energy %>%
-    gather("fuel", "cost", c("gas_cost", "electricity_cost"))
+    gather("fuel", "kwH", c("gas", "electricity"))
 
 ui <- fluidPage(
 
     titlePanel("My Energy Use"),
 
-    plotlyOutput("distPlot")
+    plotOutput("distPlot")
 )
 
 server <- function(input, output) {
 
-    output$distPlot <- renderPlotly({
-        ggplot(tidy_energy, aes(x = date, y = cost, colour = fuel)) +
+    output$distPlot <- renderPlot({
+        ggplot(tidy_energy, aes(x = date, y = kwH)) +
             geom_point() +
-            geom_smooth()
+            geom_smooth() +
+            theme_minimal() +
+            theme(legend.position = "none") +
+            facet_wrap(~ fuel, ncol = 1, scales = "free") +
+            xlab("")
     })
 }
 
