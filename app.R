4320da9aafac597866dbdd666ef24be44e0fae35
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-01-30 15:14:04 +0000

    add plots for monthly and annual bills

--- app.R
+++ app.R
@@ -21,48 +21,68 @@ if(dev_mode) {
 
   googlesheets4::gs4_deauth()
 
-  energy_2019 <- "15nKk44UVxxex7OrhdV3bZRTD0NsniclZmfwtqL0ls18" %>%
+  # should be a secret?
+  sheet_id <- "15nKk44UVxxex7OrhdV3bZRTD0NsniclZmfwtqL0ls18"
+
+  energy_2019 <- sheet_id %>%
     googlesheets4::range_read("2019")
 
-  energy_2020 <- "15nKk44UVxxex7OrhdV3bZRTD0NsniclZmfwtqL0ls18" %>%
+  energy_2020 <- sheet_id %>%
     googlesheets4::range_read("2020")
 
-  energy_2021 <- "15nKk44UVxxex7OrhdV3bZRTD0NsniclZmfwtqL0ls18" %>%
+  energy_2021 <- sheet_id %>%
     googlesheets4::range_read("2021")
 
 }
 
-energy <- bind_rows(energy_2019, energy_2020, energy_2021) %>%
-  filter(!is.na(electricity)) %>%
-  filter(!is.na(gas))
-
-energy <- energy %>%
-  impute_lm(gas ~ date) %>%
-  impute_lm(electricity ~ date)
-
-energy <- energy %>%
-  arrange(date) %>%
-  select(date, gas, electricity)
-
-tidy_energy <- energy %>%
-  gather("fuel", "kwH", c("gas", "electricity"))
-
-tidy_energy <- tidy_energy %>%
-  mutate(
-    cost = case_when(
-      fuel == "gas" ~ round(kwH * gas_rate + gas_standing, 2),
-      fuel == "electricity" ~ round(kwH * electricity_rate + electricity_standing, 2)
+prep_tidy_energy <- function(energy) {
+  energy %>%
+    filter(!is.na(electricity)) %>%
+    filter(!is.na(gas)) %>%
+    impute_lm(gas ~ date) %>%
+    impute_lm(electricity ~ date) %>%
+    arrange(date) %>%
+    select(date, gas, electricity) %>%
+    gather("fuel", "kwH", c("gas", "electricity")) %>%
+    mutate(
+      cost = case_when(
+        fuel == "gas" ~ round(kwH * gas_rate + gas_standing, 2),
+        fuel == "electricity" ~ round(kwH * electricity_rate + electricity_standing, 2)
+      )
+    ) %>%
+    group_by(fuel) %>%
+    mutate(
+      GBP = cumsum(cost)
+    ) %>%
+    mutate(
+      year = lubridate::year(date),
+      month = lubridate::month(date)
     )
+}
+
+tidy_energy <- bind_rows(energy_2019, energy_2020, energy_2021) %>%
+  prep_tidy_energy()
+
+monthly_bills <- tidy_energy %>%
+  group_by(fuel, year, month) %>%
+  summarise(
+    cost = sum(cost)
   ) %>%
-  group_by(fuel) %>%
-  mutate(
-    GBP = cumsum(cost)
-  )
+  ungroup() %>%
+  mutate(ymd = lubridate::ymd(paste(year, month, 1, sep = "-")))
+
+annual_bill <- tidy_energy %>%
+  group_by(fuel, year) %>%
+  summarise(
+    cost = sum(cost)
+  ) %>%
+  ungroup()
 
 ui = f7Page(
   title = "My Energy Use",
   dark_mode = FALSE,
   init = f7Init(skin = "ios", theme = "light"),
+  allowPWA = TRUE,
   f7TabLayout(
     navbar = f7Navbar(
       title = "My Energy Use",
@@ -104,8 +124,24 @@ ui = f7Page(
           intensity = 16,
           hover = TRUE,
           f7Card(
+            title = "Monthly bill",
+            mobileOutput("gas_monthlyPlot"),
+          )
+        ),
+        f7Shadow(
+          intensity = 16,
+          hover = TRUE,
+          f7Card(
+            title = "Annual bill",
+            mobileOutput("gas_annualPlot"),
+          )
+        ),
+        f7Shadow(
+          intensity = 16,
+          hover = TRUE,
+          f7Card(
             title = "Total cost",
-            mobileOutput("gas_costPlot"),
+            mobileOutput("gas_totalPlot"),
           )
         )
       ),
@@ -141,8 +177,24 @@ ui = f7Page(
           intensity = 16,
           hover = TRUE,
           f7Card(
+            title = "Monthly bill",
+            mobileOutput("electricity_monthlyPlot"),
+          )
+        ),
+        f7Shadow(
+          intensity = 16,
+          hover = TRUE,
+          f7Card(
+            title = "Annual bill",
+            mobileOutput("electricity_annualPlot"),
+          )
+        ),
+        f7Shadow(
+          intensity = 16,
+          hover = TRUE,
+          f7Card(
             title = "Total cost",
-            mobileOutput("electricity_costPlot"),
+            mobileOutput("electricity_totalPlot"),
           )
         )
       )
@@ -193,7 +245,7 @@ server <- function(input, output) {
         mobile_scale_y(type = "linear")
   })
 
-  output$gas_costPlot <- render_mobile({
+  output$gas_totalPlot <- render_mobile({
     tidy_energy %>%
       filter(fuel == "gas") %>%
         mobile(aes(x = date, y = GBP)) %>%
@@ -211,12 +263,46 @@ server <- function(input, output) {
         mobile_scale_y(type = "linear")
   })
 
-  output$electricity_costPlot <- render_mobile({
+  output$electricity_totalPlot <- render_mobile({
     tidy_energy %>%
       filter(fuel == "electricity") %>%
       mobile(aes(x = date, y = GBP)) %>%
         mobile_line(alpha = .5) %>%
         mobile_scale_x(type = "timeCat", tickCount = 5) %>%
+        mobile_scale_y(type = "linear")
+  })
+
+  output$electricity_monthlyPlot <- render_mobile({
+    monthly_bills %>%
+      filter(fuel == "electricity") %>%
+      mobile(aes(x = ymd, y = cost)) %>%
+        mobile_line(alpha = .5) %>%
+        mobile_scale_x(type = "timeCat", tickCount = 5) %>%
+        mobile_scale_y(type = "linear")
+  })
+
+  output$gas_monthlyPlot <- render_mobile({
+    monthly_bills %>%
+      filter(fuel == "gas") %>%
+      mobile(aes(x = ymd, y = cost)) %>%
+      mobile_line(alpha = .5) %>%
+      mobile_scale_x(type = "timeCat", tickCount = 5) %>%
+      mobile_scale_y(type = "linear")
+  })
+
+  output$electricity_annualPlot <- render_mobile({
+    annual_bill %>%
+      filter(fuel == "electricity") %>%
+      mobile(aes(x = year, y = cost)) %>%
+        mobile_line(alpha = .5) %>%
+        mobile_scale_y(type = "linear")
+  })
+
+  output$gas_annualPlot <- render_mobile({
+    annual_bill %>%
+      filter(fuel == "gas") %>%
+      mobile(aes(x = year, y = cost)) %>%
+        mobile_line(alpha = .5) %>%
         mobile_scale_y(type = "linear")
   })
 
