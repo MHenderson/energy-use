21293e3bcbc72b9a17f8ec4ef32c9f113831fa54
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-03-22 16:36:26 +0000

    download data when app starts

--- R/app_server.R
+++ R/app_server.R
@@ -6,9 +6,13 @@
 #' @noRd
 app_server <- function( input, output, session ) {
 
+  download.file("https://mjh-energy-data.netlify.app/data/tidy_energy.rds", destfile = "tidy_energy.rds")
+  tidy_energy <- readRDS("tidy_energy.rds")
   mod_usage_plot_server("usage_plot_ui", tidy_energy)
   mod_total_cost_plot_server("total_cost_plot_ui", tidy_energy)
 
+  download.file("https://mjh-energy-data.netlify.app/data/bills.rds", destfile = "bills.rds")
+  bills <- readRDS("bills.rds")
   mod_bills_plot_server("bills_plot_ui", bills)
 
   annual_summary <- tidy_energy %>%
@@ -22,6 +26,8 @@ app_server <- function( input, output, session ) {
 
   mod_annual_cost_plot_server("annual_cost_plot_ui", annual_summary)
 
+  download.file("https://mjh-energy-data.netlify.app/data/readings.rds", destfile = "readings.rds")
+  readings <- readRDS("readings.rds")
   mod_readings_plot_server("readings_plot_ui_1", readings)
 
 }
