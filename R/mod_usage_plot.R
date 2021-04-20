246a226df27765c16c4b8a9e51270964802199b1
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-04-20 14:16:36 +0000

    fix usage plot

--- R/mod_usage_plot.R
+++ R/mod_usage_plot.R
@@ -19,21 +19,26 @@ mod_usage_plot_ui <- function(id){
 #' @noRd
 mod_usage_plot_server <- function(id, tidy_energy, plot1vars){
   moduleServer( id, function(input, output, session){
+
     ns <- session$ns
+
     output$usage_plot <- dygraphs::renderDygraph({
+
       q <- tidy_energy %>%
-        dplyr::filter(var == plot1vars$var()) %>%
-        dplyr::select(fuel, date, value) %>%
-        tidyr::pivot_wider(names_from = fuel, values_from = value)
+        dplyr::filter(var == plot1vars$var(), tarrif_id %in% plot1vars$tariff()) %>%
+        dplyr::select(tarrif_id, fuel, date, value) %>%
+        dplyr::mutate(
+          id_fuel = paste0(fuel, "_", tarrif_id)
+        ) %>%
+        dplyr::select(id_fuel, date, value) %>%
+        tidyr::pivot_wider(names_from = id_fuel, values_from = value)
 
       q <- as.data.frame(q)
       xq <- xts::xts(q[,-1], order.by = q[,1])
 
       dygraphs::dygraph(xq, group = "usage") %>%
         dygraphs::dyRangeSelector(dateWindow = c("2021-01-01", "2021-04-18")) %>%
-        dygraphs::dyOptions(stepPlot = TRUE) %>%
-        dygraphs::dySeries("gas", pointSize = 2, drawPoints = TRUE, strokeWidth = 0) %>%
-        dygraphs::dySeries("electricity", pointSize = 2, drawPoints = TRUE, strokeWidth = 0) %>%
+        dygraphs::dyOptions(drawPoints = TRUE, strokeWidth = 0) %>%
         dygraphs::dyLegend(width = 300, hideOnMouseOut = FALSE) %>%
         dygraphs::dyAxis("y", label = plot1vars$var())
 
